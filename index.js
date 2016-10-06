07d9f0ffb71386756744f4c70b9eee9851304c1b
Janos Ring <Janos Ring>
2016-10-06 22:40:48 +0100

    test

--- index.js
+++ index.js
@@ -4,16 +4,6 @@ var fs = require("fs");
 
 app.set('port', (process.env.PORT || 5000));
 
-app.use(express.static(__dirname + '/public'));
-
-// views is directory for all template files
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
-
-app.get('/', function(request, response) {
-  response.render('pages/index');
-});
-
 app.get('/listUsers', function (req, res) {
    fs.readFile( __dirname + "/" + "users.json", 'utf8', function (err, data) {
       console.log( data );
